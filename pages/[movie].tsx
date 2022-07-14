c90bc62cf5fdc14f2264e76e70930900fac3f54e
harislikic <79986072+harislikic@users.noreply.github.com>
2022-07-14 13:14:17 +0200

    fix loading page data

--- pages/[movie].tsx
+++ pages/[movie].tsx
@@ -1,7 +1,7 @@
 import { useRouter } from "../node_modules/next/router";
 import React from "react";
 import { useEffect, useState } from "react";
-
+import axios from "../node_modules/axios/index";
 
 
 interface IMovie {
@@ -14,70 +14,87 @@ interface IMovie {
 }
 
 
-export default function MovieDetails (){
+function MovieDetails() {
 
     const router = useRouter()
-    
+
     console.log("Name", router.query.movie as string);
 
-    var route = router.query.movie;
 
-    
-    const [movieData, setMovieData] = useState<Partial<IMovie>>({});
-    async function getMovieByname() {
-        const responseString = await fetch("https://localhost:44300/Movies/GetMovieName?name=" + router.query.movie);
-        const response = await responseString.json();
-        setMovieData(response);
-        console.log("Response:", response);
-    }
-    console.log("Data:", movieData);
+    const API = "https://localhost:44300/Movies/GetMovieName?name=" + router.query.movie;
+
+    const [movieData, setMovieData] = useState([]);
+
 
     useEffect(() => {
-        getMovieByname();
-        console.log("mo",movieData);
+        axios
+            .get('https://localhost:44300/Movies/GetMovieName?name=Enemy')
+            .then(res => {
+                console.log(res)
+                setMovieData(res.data)
+            })
+            .catch(err => {
+                console.log(err)
+            })
+
     }, [])
-       
+
+    console.log("mo", movieData);
+    if (movieData === undefined) return <h1>Loading...</h1>
 
     return (
         <>
-            <div className="container"  >
-                <div className="movie_card" id="bright" >
-                    <div className="info_section">
-                        <div className="movie_header">
-                            <img className="locandina" src="{{x.moviePicture}}" />
-                            <h1>{`${movieData.title}`}</h1>
-                            <h4> {`${movieData.director}`}</h4>
-                            <span className="minutes">{movieData.minutes} min</span>
-                            <p> Romance </p>
-                        </div>
-                        <div className="movie_desc">
-                            <p className="text">
-                                {movieData.about}
-                            </p>
-                        </div>
 
-                        <div className="buton-div">
-                            <a className="btn" href="{x.torentLink}" rel="nofollow"><i className="fa fa-download"></i>Download </a>
 
-                        </div>
-                    </div>
+          
+                {movieData.map(value => {
+                    return (
+                        <>
 
-                    <div className="blur_back bright_back"></div>
-                </div>
+                            <div className="container"  >
+                                <div className="movie_card" id="bright" >
+                                    <div className="info_section">
+                                        <div className="movie_header">
+                                            <img className="locandina" src="{{x.moviePicture}}" />
+                                            <h1>{`${value.title}`}</h1>
+                                            <h4> {`${value.director}`}</h4>
+                                            <span className="minutes">{value.minutes} min</span>
+                                            <p> Romance </p>
+                                        </div>
+                                        <div className="movie_desc">
+                                            <p className="text">
+                                                {value.about}
+                                            </p>
+                                        </div>
 
-                <div className="yt-trailer-block" >
-                    <iframe className="yt-video" width="100%" height="100%"
-                        src={movieData.videoLink}
-                        frameBorder="0"
-                        allow="autoplay; encrypted-media"
-                        allowFullScreen
-                        title="video"
-                    />{" "}
-                </div>
-            </div>
+                                        <div className="buton-div">
+                                            <a className="btn" href="{x.torentLink}" rel="nofollow"><i className="fa fa-download"></i>Download </a>
+
+                                        </div>
+                                    </div>
+
+                                    <div className="blur_back bright_back"></div>
+                                </div>
+
+                                <div className="yt-trailer-block" >
+                                    <iframe className="yt-video" width="100%" height="100%"
+                                        src={value.videoLink}
+                                        frameBorder="0"
+                                        allow="autoplay; encrypted-media"
+                                        allowFullScreen
+                                        title="video"
+                                    />{" "}
+                                </div>
+                            </div>
+
+                        </>
+                    );
+                })}
+         
         </>
     )
 }
 
 
+export default MovieDetails
 
